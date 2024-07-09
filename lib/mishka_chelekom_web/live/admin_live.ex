cbcf2d37cf00a8fae106358829504ddeaddc9053
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-07-09 09:49:07 +0330

    VIP - Add test admin page

---
+++ lib/mishka_chelekom_web/live/admin_live.ex
@@ -0,0 +1,13 @@
+defmodule MishkaChelekomWeb.AdminLive do
+  use Phoenix.LiveView
+
+  def mount(_params, _session, socket) do
+    # Let's assume a fixed temperature for now
+    temperature = 70
+    {:ok, assign(socket, :temperature, temperature)}
+  end
+
+  def handle_event("inc", _params, socket) do
+    {:noreply, update(socket, :temperature, &(&1 + 1))}
+  end
+end
--- lib/mishka_chelekom_web/router.ex
+++ lib/mishka_chelekom_web/router.ex
@@ -18,6 +18,7 @@ defmodule MishkaChelekomWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    live "/admin", AdminLive
   end
 
   # Other scopes may use custom stacks.
