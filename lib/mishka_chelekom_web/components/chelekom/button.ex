e5ebd88fe2a63b56456627459b11409d9a7375ff
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-07-19 12:17:36 +0330

    Update button.ex

--- lib/mishka_chelekom_web/components/chelekom/button.ex
+++ lib/mishka_chelekom_web/components/chelekom/button.ex
@@ -45,13 +45,14 @@ defmodule MishkaChelekom.Button do
       id={@id}
       class={[
         "phx-submit-loading:opacity-75 inline-flex gap-2 items-center justify-center border",
-        "py-2 px-4 #{@font_weight} transition-all ease-in-ou duration-100",
+        "py-2 px-4 transition-all ease-in-ou duration-100",
         "disabled:bg-opacity-60 disabled:border-opacity-60 disabled:cursor-not-allowed",
         "disabled:cursor-not-allowed",
         "focus:outline-none",
         color_variant(@variant, @color),
         rounded_size(@rounded),
         size_class(@size),
+        @font_weight,
         @class
       ]}
       {@rest}
