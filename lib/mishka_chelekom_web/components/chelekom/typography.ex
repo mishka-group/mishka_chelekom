179839ee38bf0f19a57e1c425edf11947842f716
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-07-20 12:01:08 +0330

    Update typography.ex

--- lib/mishka_chelekom_web/components/chelekom/typography.ex
+++ lib/mishka_chelekom_web/components/chelekom/typography.ex
@@ -1,2 +1,68 @@
 defmodule MishkaChelekom.Typography do
+  use Phoenix.Component
+
+  def h1(assigns) do
+    ~H"""
+    """
+  end
+
+  def h2(assigns) do
+    ~H"""
+    """
+  end
+
+  def h3(assigns) do
+    ~H"""
+    """
+  end
+
+  def h4(assigns) do
+    ~H"""
+    """
+  end
+
+  def h5(assigns) do
+    ~H"""
+    """
+  end
+
+  def h6(assigns) do
+    ~H"""
+    """
+  end
+
+  def p(assigns) do
+    ~H"""
+    """
+  end
+
+  def ul(assigns) do
+    ~H"""
+    """
+  end
+
+  def li(assigns) do
+    ~H"""
+    """
+  end
+
+  def ol(assigns) do
+    ~H"""
+    """
+  end
+
+  def strong(assigns) do
+    ~H"""
+    """
+  end
+
+  def em(assigns) do
+    ~H"""
+    """
+  end
+
+  def blockquote(assigns) do
+    ~H"""
+    """
+  end
 end
