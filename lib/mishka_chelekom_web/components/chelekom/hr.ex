577e871721756f3bceff7967daf5a2c19e984d62
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-07-21 15:04:33 +0330

    VIP - Add hr entry module

--- lib/mishka_chelekom_web/components/chelekom/hr.ex
+++ lib/mishka_chelekom_web/components/chelekom/hr.ex
@@ -1,2 +1,29 @@
 defmodule MishkaChelekom.Hr do
+  use Phoenix.Component
+
+  @colors [
+    "white",
+    "primary",
+    "secondary",
+    "dark",
+    "success",
+    "warning",
+    "danger",
+    "info",
+    "light",
+    "misc",
+    "dawn"
+  ]
+
+  @doc type: :component
+  attr :id, :string, default: nil, doc: ""
+  attr :type, :string, values: ["dashed", "solid"], default: "dashed", doc: ""
+  attr :color, :string, values: @colors, default: "white", doc: ""
+  attr :size, :string, default: "large", doc: ""
+  attr :class, :string, default: nil, doc: ""
+
+  def hr(assigns) do
+    ~H"""
+    """
+  end
 end
