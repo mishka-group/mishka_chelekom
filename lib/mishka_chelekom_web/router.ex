f19a888de867af4bcdc3cede5f3cca28e1671947
Mona Aghili <28711325+MonaAghili@users.noreply.github.com>
2024-07-23 00:50:52 +0330

    add vertical examples

--- lib/mishka_chelekom_web/router.ex
+++ lib/mishka_chelekom_web/router.ex
@@ -30,6 +30,7 @@ defmodule MishkaChelekomWeb.Router do
 
     live "/examples/button", Examples.ButtonLive
     live "/examples/typography", Examples.TypographyLive
+    live "/examples/hr", Examples.HrLive
   end
 
   # Other scopes may use custom stacks.
