0cbef46081e66d273a081694dce8b998a7957b63
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-07-22 09:58:56 +0330

    VIP - Add entry module of avatar

--- lib/mishka_chelekom_web/components/chelekom/avatar.ex
+++ lib/mishka_chelekom_web/components/chelekom/avatar.ex
@@ -1,2 +1,44 @@
 defmodule MishkaChelekom.Avatar do
+  use Phoenix.Component
+
+  # TODO: We need Avatar tooltip
+  # TODO: We need Dot indicator
+  # TODO: We need dropdown
+  @sizes ["extra_small", "small", "medium", "large", "extra_large"]
+
+  @doc type: :component
+  attr :id, :string, default: nil, doc: ""
+
+  attr :type, :string,
+    values: ["default", "placeholder", "placeholder_icon", nil],
+    default: "default",
+    doc: ""
+
+  attr :size, :string, default: "large", doc: ""
+  attr :rounded, :string, values: @sizes ++ ["full", "none"], default: "large", doc: ""
+  attr :border, :string, doc: ""
+  attr :rest, :global
+
+  def avatar(assigns) do
+    ~H"""
+    """
+  end
+
+  @doc type: :component
+  attr :id, :string, default: nil, doc: ""
+
+  attr :type, :string,
+    values: ["default", "placeholder", "placeholder_icon", nil],
+    default: "default",
+    doc: ""
+
+  attr :size, :string, default: "large", doc: ""
+  attr :rounded, :string, values: @sizes ++ ["full", "none"], default: "large", doc: ""
+  attr :border, :string, doc: ""
+  attr :rest, :global
+
+  def avatar_group(assigns) do
+    ~H"""
+    """
+  end
 end
