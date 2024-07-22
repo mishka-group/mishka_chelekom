63e8c0b3ac0d93c7a0634eafec00f5599a04cfa8
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-07-22 10:36:22 +0330

    VIP - Add entry module of banner

--- lib/mishka_chelekom_web/components/chelekom/banner.ex
+++ lib/mishka_chelekom_web/components/chelekom/banner.ex
@@ -1,2 +1,37 @@
 defmodule MishkaChelekom.Banner do
+  use Phoenix.Component
+  @sizes ["extra_small", "small", "medium", "large", "extra_large"]
+  @colors [
+    "white",
+    "primary",
+    "secondary",
+    "dark",
+    "success",
+    "warning",
+    "danger",
+    "info",
+    "light",
+    "misc",
+    "dawn"
+  ]
+  @doc type: :component
+  attr :id, :string, default: nil, doc: ""
+
+  attr :variant, :string,
+    values: ["default", "spaced"],
+    default: "default",
+    doc: ""
+
+  attr :size, :string, default: "large", doc: ""
+  attr :border, :string, doc: ""
+  attr :color, :string, values: @colors, default: "white", doc: ""
+  attr :position, :string, values: ["top", "bottom"], default: "top", doc: ""
+  attr :rounded, :string, values: @sizes ++ ["full", "none"], default: "large", doc: ""
+  attr :rest, :global, include: ~w(right_dismiss left_dismiss), doc: ""
+  slot :inner_block, required: false, doc: ""
+
+  def banner(assigns) do
+    ~H"""
+    """
+  end
 end
