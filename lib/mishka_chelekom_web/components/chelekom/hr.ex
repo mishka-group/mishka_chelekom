35e0f81a1bbfaaab3e94f243843a68efe909e79d
Mona Aghili <28711325+MonaAghili@users.noreply.github.com>
2024-07-23 21:01:52 +0330

    reset border colors

--- lib/mishka_chelekom_web/components/chelekom/hr.ex
+++ lib/mishka_chelekom_web/components/chelekom/hr.ex
@@ -111,23 +111,23 @@ defmodule MishkaChelekom.Hr do
   end
 
   defp color_class("success") do
-    "border-[#ECFEF3] text-[#047857]"
+    "border-[#227A52] text-[#047857]"
   end
 
   defp color_class("warning") do
-    "border-[#FFF8E6] text-[#FF8B08]"
+    "border-[#FF8B08] text-[#FF8B08]"
   end
 
   defp color_class("danger") do
-    "border-[#FFE6E6] text-[#E73B3B]"
+    "border-[#E73B3B] text-[#E73B3B]"
   end
 
   defp color_class("info") do
-    "border-[#E5F0FF] text-[#004FC4]"
+    "border-[#6663FD] text-[#004FC4]"
   end
 
   defp color_class("misc") do
-    "border-[#FFE6FF] text-[#52059C]"
+    "border-[#52059C] text-[#52059C]"
   end
 
   defp color_class("dawn") do
@@ -135,7 +135,7 @@ defmodule MishkaChelekom.Hr do
   end
 
   defp color_class("light") do
-    "border-[#E3E7F1] text-[#707483]"
+    "border-[#707483] text-[#707483]"
   end
 
   defp color_class("dark") do
