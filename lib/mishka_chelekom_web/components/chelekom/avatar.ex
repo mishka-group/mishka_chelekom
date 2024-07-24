a5c7e4e101c397ab3887b8f8f5570fa309deebc4
Mona Aghili <28711325+MonaAghili@users.noreply.github.com>
2024-07-25 00:21:25 +0330

    added all variants of badge

--- lib/mishka_chelekom_web/components/chelekom/avatar.ex
+++ lib/mishka_chelekom_web/components/chelekom/avatar.ex
@@ -35,6 +35,11 @@ defmodule MishkaChelekom.Avatar do
 
   def avatar(assigns) do
     ~H"""
+    <img
+      src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+      class="size-8 rounded-full shadow-sm border border-neutral-500 border-opacity-25"
+      alt=""
+    />
     """
   end
 
@@ -55,4 +60,61 @@ defmodule MishkaChelekom.Avatar do
     ~H"""
     """
   end
+
+  defp color("white") do
+    "text-white"
+  end
+
+  defp color("primary") do
+    "text-[#4363EC]"
+  end
+
+  defp color("secondary") do
+    "text-[#6B6E7C]"
+  end
+
+  defp color("success") do
+    "text-[#227A52]"
+  end
+
+  defp color("warning") do
+    "text-[#FF8B08]"
+  end
+
+  defp color("danger") do
+    "text-[#E73B3B]"
+  end
+
+  defp color("info") do
+    "text-[#6663FD]"
+  end
+
+  defp color("misc") do
+    "text-[#52059C]"
+  end
+
+  defp color("dawn") do
+    "text-[#4D4137]"
+  end
+
+  defp color("light") do
+    "text-[#707483]"
+  end
+
+  defp color("dark") do
+    "text-[#1E1E1E]"
+  end
+
+  defp color(params), do: params
+
+  defp size_class("extra_small"), do: "text-xs"
+  defp size_class("small"), do: "text-sm"
+  defp size_class("medium"), do: "text-base"
+  defp size_class("large"), do: "text-lg"
+  defp size_class("extra_large"), do: "text-xl"
+  defp size_class("double_large"), do: "text-2xl"
+  defp size_class("triple_large"), do: "text-3xl"
+  defp size_class("quadruple_large"), do: "text-4xl"
+  defp size_class(params) when is_binary(params), do: params
+  defp size_class(_), do: size_class("medoum")
 end
--- lib/mishka_chelekom_web/components/chelekom/badge.ex
+++ lib/mishka_chelekom_web/components/chelekom/badge.ex
@@ -21,7 +21,7 @@ defmodule MishkaChelekom.Badge do
   attr :id, :string, default: nil, doc: ""
 
   attr :variant, :string,
-    values: ["default", "bordered"],
+    values: ["default", "outline", "transparent","unbordered", "shadow"],
     default: "default",
     doc: ""
 
@@ -111,49 +111,183 @@ defmodule MishkaChelekom.Badge do
     "bg-[#1E1E1E] text-white border-[#1E1E1E] hover:bg-[#111111] hover:border-[#111111]"
   end
 
-  defp color_variant("bordered", "white") do
+  defp color_variant("outline", "white") do
     "bg-transparent text-white border-white hover:text-[#ededed] hover:border-[#d9d9d9]"
   end
 
-  defp color_variant("bordered", "primary") do
+  defp color_variant("outline", "primary") do
     "bg-transparent text-[#4363EC] border-[#4363EC] hover:text-[#072ed3] hover:border-[#072ed3]"
   end
 
-  defp color_variant("bordered", "secondary") do
+  defp color_variant("outline", "secondary") do
     "bg-transparent text-[#6B6E7C] border-[#6B6E7C] hover:text-[#60636f] hover:border-[#60636f]"
   end
 
-  defp color_variant("bordered", "success") do
+  defp color_variant("outline", "success") do
     "bg-transparent text-[#227A52] border-[#227A52] hover:text-[#d4fde4] hover:border-[#d4fde4]"
   end
 
-  defp color_variant("bordered", "warning") do
+  defp color_variant("outline", "warning") do
     "bg-transparent text-[#FF8B08] border-[#FF8B08] hover:text-[#fff1cd] hover:border-[#fff1cd]"
   end
 
-  defp color_variant("bordered", "danger") do
+  defp color_variant("outline", "danger") do
     "bg-transparent text-[#E73B3B] border-[#E73B3B] hover:text-[#ffcdcd] hover:border-[#ffcdcd]"
   end
 
-  defp color_variant("bordered", "info") do
+  defp color_variant("outline", "info") do
     "bg-transparent text-[#004FC4] border-[#004FC4] hover:text-[#cce1ff] hover:border-[#cce1ff]"
   end
 
-  defp color_variant("bordered", "misc") do
+  defp color_variant("outline", "misc") do
     "bg-transparent text-[#52059C] border-[#52059C] hover:text-[#ffe0ff] hover:border-[#ffe0ff]"
   end
 
-  defp color_variant("bordered", "dawn") do
+  defp color_variant("outline", "dawn") do
     "bg-transparent text-[#4D4137] border-[#4D4137] hover:text-[#FFECDA] hover:border-[#FFECDA]"
   end
 
-  defp color_variant("bordered", "light") do
+  defp color_variant("outline", "light") do
     "bg-transparent text-[#707483] border-[#707483] hover:text-[#d2d8e9] hover:border-[#d2d8e9]"
   end
 
-  defp color_variant("bordered", "dark") do
+  defp color_variant("outline", "dark") do
     "bg-transparent text-[#1E1E1E] border-[#1E1E1E] hover:text-[#111111] hover:border-[#111111]"
   end
+
+  defp color_variant("unbordered", "white") do
+    "bg-white text-[#3E3E3E] border-transparent hover:bg-[#ededed]"
+  end
+
+  defp color_variant("unbordered", "primary") do
+    "bg-[#4363EC] text-white border-transparent hover:bg-[#072ed3]"
+  end
+
+  defp color_variant("unbordered", "secondary") do
+    "bg-[#6B6E7C] text-white border-transparent hover:bg-[#60636f]"
+  end
+
+  defp color_variant("unbordered", "success") do
+    "bg-[#ECFEF3] text-[#047857] border-transparent hover:bg-[#d4fde4]"
+  end
+
+  defp color_variant("unbordered", "warning") do
+    "bg-[#FFF8E6] text-[#FF8B08] border-transparent hover:bg-[#fff1cd]"
+  end
+
+  defp color_variant("unbordered", "danger") do
+    "bg-[#FFE6E6] text-[#E73B3B] border-transparent hover:bg-[#ffcdcd]"
+  end
+
+  defp color_variant("unbordered", "info") do
+    "bg-[#E5F0FF] text-[#004FC4] border-transparent hover:bg-[#cce1ff]"
+  end
+
+  defp color_variant("unbordered", "misc") do
+    "bg-[#FFE6FF] text-[#52059C] border-transparent hover:bg-[#ffe0ff]"
+  end
+
+  defp color_variant("unbordered", "dawn") do
+    "bg-[#FFECDA] text-[#4D4137] border-transparent hover:bg-[#ffdfc1]"
+  end
+
+  defp color_variant("unbordered", "light") do
+    "bg-[#E3E7F1] text-[#707483] border-transparent hover:bg-[#d2d8e9]"
+  end
+
+  defp color_variant("unbordered", "dark") do
+    "bg-[#1E1E1E] text-white border-transparent hover:bg-[#111111]"
+  end
+
+  defp color_variant("transparent", "white") do
+    "bg-transparent text-white border-transparent hover:text-[#ededed]"
+  end
+
+  defp color_variant("transparent", "primary") do
+    "bg-transparent text-[#4363EC] border-transparent hover:text-[#072ed3]"
+  end
+
+  defp color_variant("transparent", "secondary") do
+    "bg-transparent text-[#6B6E7C] border-transparent hover:text-[#60636f]"
+  end
+
+  defp color_variant("transparent", "success") do
+    "bg-transparent text-[#227A52] border-transparent hover:text-[#d4fde4]"
+  end
+
+  defp color_variant("transparent", "warning") do
+    "bg-transparent text-[#FF8B08] border-transparent hover:text-[#fff1cd]"
+  end
+
+  defp color_variant("transparent", "danger") do
+    "bg-transparent text-[#E73B3B] border-transparent hover:text-[#ffcdcd]"
+  end
+
+  defp color_variant("transparent", "info") do
+    "bg-transparent text-[#6663FD] border-transparent hover:text-[#cce1ff]"
+  end
+
+  defp color_variant("transparent", "misc") do
+    "bg-transparent text-[#52059C] border-transparent hover:text-[#ffe0ff]"
+  end
+
+  defp color_variant("transparent", "dawn") do
+    "bg-transparent text-[#4D4137] border-transparent hover:text-[#FFECDA]"
+  end
+
+  defp color_variant("transparent", "light") do
+    "bg-transparent text-[#707483] border-transparent hover:text-[#d2d8e9]"
+  end
+
+  defp color_variant("transparent", "dark") do
+    "bg-transparent text-[#1E1E1E] border-transparent hover:text-[#111111]"
+  end
+
+  defp color_variant("shadow", "white") do
+    "bg-white text-[#3E3E3E] border-[#DADADA] shadow hover:bg-[#ededed] hover:border-[#d9d9d9]"
+  end
+
+  defp color_variant("shadow", "primary") do
+    "bg-[#4363EC] text-white border-[#4363EC] shadow hover:bg-[#072ed3] hover:border-[#072ed3]"
+  end
+
+  defp color_variant("shadow", "secondary") do
+    "bg-[#6B6E7C] text-white border-[#6B6E7C] shadow hover:bg-[#60636f] hover:border-[#60636f]"
+  end
+
+  defp color_variant("shadow", "success") do
+    "bg-[#AFEAD0] text-[#227A52] border-[#AFEAD0] shadow hover:bg-[#d4fde4] hover:border-[#d4fde4]"
+  end
+
+  defp color_variant("shadow", "warning") do
+    "bg-[#FFF8E6] text-[#FF8B08] border-[#FFF8E6] shadow hover:bg-[#fff1cd] hover:border-[#fff1cd]"
+  end
+
+  defp color_variant("shadow", "danger") do
+    "bg-[#FFE6E6] text-[#E73B3B] border-[#FFE6E6] shadow hover:bg-[#ffcdcd] hover:border-[#ffcdcd]"
+  end
+
+  defp color_variant("shadow", "info") do
+    "bg-[#E5F0FF] text-[#004FC4] border-[#E5F0FF] shadow hover:bg-[#cce1ff] hover:border-[#cce1ff]"
+  end
+
+  defp color_variant("shadow", "misc") do
+    "bg-[#FFE6FF] text-[#52059C] border-[#FFE6FF] shadow hover:bg-[#ffe0ff] hover:border-[#ffe0ff]"
+  end
+
+  defp color_variant("shadow", "dawn") do
+    "bg-[#FFECDA] text-[#4D4137] border-[#FFECDA] shadow hover:bg-[#ffdfc1] hover:border-[#ffdfc1]"
+  end
+
+  defp color_variant("shadow", "light") do
+    "bg-[#E3E7F1] text-[#707483] border-[#E3E7F1] shadow hover:bg-[#d2d8e9] hover:border-[#d2d8e9]"
+  end
+
+  defp color_variant("shadow", "dark") do
+    "bg-[#1E1E1E] text-white border-[#1E1E1E] shadow hover:bg-[#111111] hover:border-[#111111]"
+  end
+
+  defp color_variant(_, _), do: color_variant("default", "white")
 
   defp rounded_size("extra_small"), do: "rounded-sm"
   defp rounded_size("small"), do: "rounded"
