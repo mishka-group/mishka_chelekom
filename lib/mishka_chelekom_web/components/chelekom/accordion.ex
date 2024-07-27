9daced49239ca7b839a4dc360757e42ff2b353dd
Mona Aghili <28711325+MonaAghili@users.noreply.github.com>
2024-07-27 12:37:55 +0330

    remove svg as indicator

--- lib/mishka_chelekom_web/components/chelekom/accordion.ex
+++ lib/mishka_chelekom_web/components/chelekom/accordion.ex
@@ -33,7 +33,55 @@ defmodule MishkaChelekom.Accordion do
   defp space_class("extra_large"), do: "space-y-6"
   defp space_class("none"), do: "space-x-0"
   defp space_class(params) when is_binary(params), do: params
-  defp space_class(_), do: space_class("small")
+  defp space_class(_), do: space_class("small")\
+
+  defp border("white") do
+    "border-[#DADADA] hover:border-[#d9d9d9]"
+  end
+
+  defp border("transparent") do
+    "border-transparent"
+  end
+
+  defp border("primary") do
+    "border-[#4363EC] hover:border-[#072ed3]"
+  end
+
+  defp border("secondary") do
+    "border-[#6B6E7C] hover:border-[#60636f]"
+  end
+
+  defp border("success") do
+    "border-[#227A52] hover:border-[#d4fde4]"
+  end
+
+  defp border("warning") do
+    "border-[#FF8B08] hover:border-[#fff1cd]"
+  end
+
+  defp border("danger") do
+    "border-[#E73B3B] hover:border-[#ffcdcd]"
+  end
+
+  defp border("info") do
+    "border-[#004FC4] hover:border-[#cce1ff]"
+  end
+
+  defp border("misc") do
+    "border-[#52059C] hover:border-[#ffe0ff]"
+  end
+
+  defp border("dawn") do
+    "border-[#4D4137] hover:border-[#FFECDA]"
+  end
+
+  defp border("light") do
+    "border-[#707483] hover:border-[#d2d8e9]"
+  end
+
+  defp border("dark") do
+    "border-[#1E1E1E] hover:border-[#111111]"
+  end
 
   defp default_classes() do
     [
