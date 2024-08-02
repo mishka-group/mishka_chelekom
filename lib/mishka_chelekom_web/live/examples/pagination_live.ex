35865ba73ba1d730277dc7ed586e92bf863ea2ef
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-08-02 09:43:41 +0330

    VIP - Add live functions

--- lib/mishka_chelekom_web/live/examples/pagination_live.ex
+++ lib/mishka_chelekom_web/live/examples/pagination_live.ex
@@ -1,8 +1,21 @@
 defmodule MishkaChelekomWeb.Examples.PaginationLive do
   use Phoenix.LiveView
   use Phoenix.Component
+  import MishkaChelekom.Pagination
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, :posts, %{total: 20, active: 1})}
+  end
+
+  def handle_event("pagination", %{"action" => "next"} = _params, socket) do
+    {:noreply, assign(socket, :posts, %{total: 20, active: socket.assigns.posts.active + 1})}
+  end
+
+  def handle_event("pagination", %{"action" => "select", "page" => page} = _params, socket) do
+    {:noreply, assign(socket, :posts, %{total: 20, active: page})}
+  end
+
+  def handle_event("pagination", %{"action" => "previous"} = _params, socket) do
+    {:noreply, assign(socket, :posts, %{total: 20, active: socket.assigns.posts.active - 1})}
   end
 end
