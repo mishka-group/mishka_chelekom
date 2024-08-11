1354dab2bd76dc32072d4ad89fb3cf92244a5ac8
Mona Aghili <28711325+MonaAghili@users.noreply.github.com>
2024-08-11 20:29:39 +0330

    add icon

--- lib/mishka_chelekom_web/components/chelekom/blockquote.ex
+++ lib/mishka_chelekom_web/components/chelekom/blockquote.ex
@@ -42,6 +42,9 @@ defmodule MishkaChelekom.Blockquote do
   def blockquote(assigns) do
     ~H"""
     <figure class="space-y-3">
+      <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
+        <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"/>
+      </svg>
       <blockquote class="border-s-4 border-gray-300 bg-gray-50 p-2 italic">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem nihil commodi, facere voluptatum dolores tempora vero soluta harum nam esse
       </blockquote>
