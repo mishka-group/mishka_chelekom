b4562344193d130bdc3df9be94797098d1fe735e
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-08-11 20:51:25 +0330

    VIP - Add blockquote options

--- lib/mishka_chelekom_web/components/chelekom/blockquote.ex
+++ lib/mishka_chelekom_web/components/chelekom/blockquote.ex
@@ -32,8 +32,10 @@ defmodule MishkaChelekom.Blockquote do
   attr :class, :string, default: nil, doc: ""
   attr :rest, :global, doc: ""
 
-  slot :title, required: false do
-    attr :icon, :string, required: false
+  slot :caption, required: false do
+    attr :image, :string
+    attr :image_class, :string
+    attr :position, :string, values: ["right", "left", "ceneter"]
   end
 
   slot :content, required: false
@@ -42,20 +44,49 @@ defmodule MishkaChelekom.Blockquote do
   def blockquote(assigns) do
     ~H"""
     <figure class="space-y-3">
-      <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
-        <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"/>
-      </svg>
+      <.blockquote_icon />
       <blockquote class="border-s-4 border-gray-300 bg-gray-50 p-2 italic">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem nihil commodi, facere voluptatum dolores tempora vero soluta harum nam esse
+        <%= render_slot(@inner_block) %>
       </blockquote>
-      <figcaption class="flex items-center justify-center mt-6 space-x-3 rtl:space-x-reverse">
-          <img class="w-6 h-6 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="">
-          <div class="flex items-center divide-x-2 rtl:divide-x-reverse divide-gray-500">
-            <cite class="pe-3">Text</cite>
-            <cite class="ps-3">Text</cite>
-          </div>
+      <figcaption
+        :for={caption <- @caption}
+        class={[
+          "flex items-center justify-center mt-6 space-x-3 rtl:space-x-reverse",
+          caption_position(caption[:position])
+        ]}
+      >
+        <img
+          :if={!is_nil(caption[:image])}
+          class={["w-6 h-6 rounded-full", caption[:image_class]]}
+          src={caption[:image]}
+        />
+        <div class="flex items-center divide-x-2 rtl:divide-x-reverse divide-gray-500">
+          <%= render_slot(caption) %>
+        </div>
       </figcaption>
     </figure>
     """
   end
+
+  defp blockquote_icon(assigns) do
+    ~H"""
+    <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
+      <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
+    </svg>
+    """
+  end
+
+  defp caption_position("right") do
+    "right class"
+  end
+
+  defp caption_position("left") do
+    "left class"
+  end
+
+  defp caption_position("center") do
+    "center class"
+  end
+
+  defp caption_position(_), do: caption_position("right")
 end
