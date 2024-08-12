434f8067e29da90261ea339e88cde34aeeb3f2e0
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-08-12 22:04:34 +0330

    vip

--- lib/mishka_chelekom_web/live/examples/list_live.ex
+++ lib/mishka_chelekom_web/live/examples/list_live.ex
@@ -1,6 +1,7 @@
 defmodule MishkaChelekomWeb.Examples.ListLive do
   use Phoenix.LiveView
   use Phoenix.Component
+  alias Phoenix.LiveView.JS
 
   def mount(_params, _session, socket) do
     {:ok, socket}
