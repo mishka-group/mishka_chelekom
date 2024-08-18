a3865f5ac05c459b2ed5e77e5bb8ad5fa936c8f2
Mona Aghili <28711325+MonaAghili@users.noreply.github.com>
2024-08-18 12:21:55 +0330

    add opacity backdrop

--- lib/mishka_chelekom_web/components/chelekom/overlay.ex
+++ lib/mishka_chelekom_web/components/chelekom/overlay.ex
@@ -17,10 +17,13 @@ defmodule MishkaChelekom.Overlay do
 
   @doc type: :component
   attr :id, :string, default: nil, doc: ""
-  attr :color, :string, values: @colors, default: "dark", doc: ""
-  attr :opacity, :integer, default: 100, doc: ""
+  attr :color, :string, values: @colors, default: "white", doc: ""
+  attr :opacity, :string, default: "100", doc: ""
+  attr :backdrop, :string, default: "none", doc: ""
   attr :class, :string, default: nil, doc: ""
   attr :rest, :global, doc: ""
+
+  slot :inner_block, required: false, doc: ""
 
   def overlay(assigns) do
     ~H"""
@@ -28,57 +31,57 @@ defmodule MishkaChelekom.Overlay do
       id={@id}
       class={[
         "absolute inset-0",
-        color_class(@color, @opacity),
+        color_class(@color, @opacity, @backdrop),
         @class
       ]}
       {@rest}
     >
-
+      <%= render_slot(@inner_block) %>
     </div>
     """
   end
 
-  defp color_class("white", opacity) do
-    "bg-white/#{opacity}"
+  defp color_class("white", opacity, backdrop) do
+    "bg-white/#{opacity} backdrop-blur-#{backdrop}"
   end
 
-  defp color_class("primary", opacity) do
-    "bg-[#2441de]/#{opacity}"
+  defp color_class("primary", opacity, backdrop) do
+    "bg-[#2441de]/#{opacity} backdrop-blur-#{backdrop}"
   end
 
-  defp color_class("secondary", opacity) do
-    "bg-[#877C7C]/#{opacity}"
+  defp color_class("secondary", opacity, backdrop) do
+    "bg-[#877C7C]/#{opacity} backdrop-blur-#{backdrop}"
   end
 
-  defp color_class("success", opacity) do
-    "bg-[#6EE7B7]/#{opacity}"
+  defp color_class("success", opacity, backdrop) do
+    "bg-[#6EE7B7]/#{opacity} backdrop-blur-#{backdrop}"
   end
 
-  defp color_class("warning", opacity) do
-    "bg-[#FF8B08]/#{opacity}"
+  defp color_class("warning", opacity, backdrop) do
+    "bg-[#FF8B08]/#{opacity} backdrop-blur-#{backdrop}"
   end
 
-  defp color_class("danger", opacity) do
-    "bg-[#E73B3B]/#{opacity}"
+  defp color_class("danger", opacity, backdrop) do
+    "bg-[#E73B3B]/#{opacity} backdrop-blur-#{backdrop}"
   end
 
-  defp color_class("info", opacity) do
-    "bg-[#004FC4]/#{opacity}"
+  defp color_class("info", opacity, backdrop) do
+    "bg-[#004FC4]/#{opacity} backdrop-blur-#{backdrop}"
   end
 
-  defp color_class("misc", opacity) do
-    "bg-[#52059C]/#{opacity}"
+  defp color_class("misc", opacity, backdrop) do
+    "bg-[#52059C]/#{opacity} backdrop-blur-#{backdrop}"
   end
 
-  defp color_class("dawn", opacity) do
-    "bg-[#4D4137]/#{opacity}"
+  defp color_class("dawn", opacity, backdrop) do
+    "bg-[#4D4137]/#{opacity} backdrop-blur-#{backdrop}"
   end
 
-  defp color_class("light", opacity) do
-    "bg-[#707483]/#{opacity}"
+  defp color_class("light", opacity, backdrop) do
+    "bg-[#707483]/#{opacity} backdrop-blur-#{backdrop}"
   end
 
-  defp color_class("dark", opacity) do
-    "bg-[#050404]/#{opacity}"
+  defp color_class("dark", opacity, backdrop) do
+    "bg-[#050404]/#{opacity} backdrop-blur-#{backdrop}"
   end
 end
