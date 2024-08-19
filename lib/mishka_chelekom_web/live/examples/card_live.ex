e460473c1f43bb95ff1cce87f3f4880202fb2f0e
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-08-19 21:38:54 +0330

    VIP

--- lib/mishka_chelekom_web/live/examples/card_live.ex
+++ lib/mishka_chelekom_web/live/examples/card_live.ex
@@ -2,6 +2,7 @@ defmodule MishkaChelekomWeb.Examples.CardLive do
   use Phoenix.LiveView
   use Phoenix.Component
   import MishkaChelekomComponents
+  import MishkaChelekom.{Card, Button, Badge}
 
   def mount(_params, _session, socket) do
     {:ok, socket}
