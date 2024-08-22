8c009ab59a2d85faaf648bf2e85823b0a2a67c5a
Mona Aghili <28711325+MonaAghili@users.noreply.github.com>
2024-08-22 23:13:28 +0330

    Fix issue of wrong component

--- lib/mishka_chelekom_web/components/chelekom/drawer.ex
+++ lib/mishka_chelekom_web/components/chelekom/drawer.ex
@@ -1,13 +1,346 @@
 defmodule MishkaChelekom.Drawer do
   use Phoenix.Component
+  import MishkaChelekomComponents
+
+  @sizes [
+    "extra_small",
+    "small",
+    "medium",
+    "large",
+    "extra_large",
+    "double_large",
+    "triple_large",
+    "quadruple_large"
+  ]
+  @colors [
+    "white",
+    "primary",
+    "secondary",
+    "dark",
+    "success",
+    "warning",
+    "danger",
+    "info",
+    "light",
+    "misc",
+    "dawn"
+  ]
+
+  @variants [
+    "default",
+    "outline",
+    "transparent",
+    "shadow",
+    "unbordered"
+  ]
 
   @doc type: :component
   attr :id, :string, default: nil, doc: ""
+  attr :variant, :string, values: @variants, default: "default", doc: ""
+  attr :color, :string, values: @colors, default: "white", doc: ""
+  attr :size, :string, default: "large", doc: ""
+  attr :border, :string, values: @sizes ++ [nil], default: "extra_small", doc: ""
+  attr :rounded, :string, values: @sizes ++ [nil], default: nil, doc: ""
+  attr :space, :string, values: @sizes ++ [nil], default: nil, doc: ""
+  attr :padding, :string, default: "none", doc: ""
   attr :class, :string, default: nil, doc: ""
   attr :rest, :global, doc: ""
 
   def drawer(assigns) do
     ~H"""
+    <div
+      id={@id}
+      class={[
+        "fixed top-0 left-0 z-40 h-screen p-2 overflow-y-auto transition-transform -translate-x-full",
+        size_class(@sizes),
+        color_variant(@variant, @color)
+      ]}
+      tabindex="-1"
+      aria-labelledby="drawer-label"
+    >
+      <div class="flex justify-between items-center gap-5">
+        <h5
+          id="drawer-label"
+          class="inline-flex items-center mb-4 text-base font-semibold text-gray-500 dark:text-gray-400"
+        >
+          Info
+        </h5>
+        <button
+          type="button"
+          data-drawer-hide="drawer-example"
+          aria-controls="drawer-example"
+          class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white"
+        >
+          <.icon name="x-mark" />
+          <span class="sr-only">Close menu</span>
+        </button>
+      </div>
+
+      <p class="mb-6 text-sm text-gray-500 dark:text-gray-400">
+        Supercharge your hiring by taking advantage of our
+        <a href="#" class="text-blue-600 underline dark:text-blue-500 hover:no-underline">
+          limited-time sale
+        </a>
+        for Flowbite Docs + Job Board. Unlimited access to over 190K top-ranked candidates and the #1 design job board.
+      </p>
+      <div class="grid grid-cols-2 gap-4">
+        <a
+          href="#"
+          class="px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-200 rounded-lg focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+        >
+          Learn more
+        </a>
+        <a
+          href="#"
+          class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        >
+          Get access
+          <svg
+            class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </a>
+      </div>
+    </div>
     """
+  end
+
+
+  defp size_class("extra_small"), do: "w-60"
+  defp size_class("small"), do: "w-64"
+  defp size_class("medium"), do: "w-72"
+  defp size_class("large"), do: "w-80"
+  defp size_class("extra_large"), do: "w-96"
+  defp size_class(_), do: size_class("large")
+
+  defp color_variant("default", "white") do
+    "bg-white text-[#3E3E3E] border-[#DADADA]"
+  end
+
+  defp color_variant("default", "primary") do
+    "bg-[#4363EC] text-white border-[#2441de]"
+  end
+
+  defp color_variant("default", "secondary") do
+    "bg-[#6B6E7C] text-white border-[#877C7C]"
+  end
+
+  defp color_variant("default", "success") do
+    "bg-[#ECFEF3] text-[#047857] border-[#6EE7B7]"
+  end
+
+  defp color_variant("default", "warning") do
+    "bg-[#FFF8E6] text-[#FF8B08] border-[#FF8B08]"
+  end
+
+  defp color_variant("default", "danger") do
+    "bg-[#FFE6E6] text-[#E73B3B] border-[#E73B3B]"
+  end
+
+  defp color_variant("default", "info") do
+    "bg-[#E5F0FF] text-[#004FC4] border-[#004FC4]"
+  end
+
+  defp color_variant("default", "misc") do
+    "bg-[#FFE6FF] text-[#52059C] border-[#52059C]"
+  end
+
+  defp color_variant("default", "dawn") do
+    "bg-[#FFECDA] text-[#4D4137] border-[#4D4137]"
+  end
+
+  defp color_variant("default", "light") do
+    "bg-[#E3E7F1] text-[#707483] border-[#707483]"
+  end
+
+  defp color_variant("default", "dark") do
+    "bg-[#1E1E1E] text-white border-[#050404]"
+  end
+
+  defp color_variant("outline", "white") do
+    "bg-transparent text-white border-white"
+  end
+
+  defp color_variant("outline", "primary") do
+    "bg-transparent text-[#4363EC] border-[#4363EC] "
+  end
+
+  defp color_variant("outline", "secondary") do
+    "bg-transparent text-[#6B6E7C] border-[#6B6E7C]"
+  end
+
+  defp color_variant("outline", "success") do
+    "bg-transparent text-[#227A52] border-[#6EE7B7]"
+  end
+
+  defp color_variant("outline", "warning") do
+    "bg-transparent text-[#FF8B08] border-[#FF8B08]"
+  end
+
+  defp color_variant("outline", "danger") do
+    "bg-transparent text-[#E73B3B] border-[#E73B3B]"
+  end
+
+  defp color_variant("outline", "info") do
+    "bg-transparent text-[#004FC4] border-[#004FC4]"
+  end
+
+  defp color_variant("outline", "misc") do
+    "bg-transparent text-[#52059C] border-[#52059C]"
+  end
+
+  defp color_variant("outline", "dawn") do
+    "bg-transparent text-[#4D4137] border-[#4D4137]"
+  end
+
+  defp color_variant("outline", "light") do
+    "bg-transparent text-[#707483] border-[#707483]"
+  end
+
+  defp color_variant("outline", "dark") do
+    "bg-transparent text-[#1E1E1E] border-[#1E1E1E]"
+  end
+
+  defp color_variant("unbordered", "white") do
+    "bg-white text-[#3E3E3E] border-transparent"
+  end
+
+  defp color_variant("unbordered", "primary") do
+    "bg-[#4363EC] text-white border-transparent"
+  end
+
+  defp color_variant("unbordered", "secondary") do
+    "bg-[#6B6E7C] text-white border-transparent"
+  end
+
+  defp color_variant("unbordered", "success") do
+    "bg-[#ECFEF3] text-[#047857] border-transparent"
+  end
+
+  defp color_variant("unbordered", "warning") do
+    "bg-[#FFF8E6] text-[#FF8B08] border-transparent"
+  end
+
+  defp color_variant("unbordered", "danger") do
+    "bg-[#FFE6E6] text-[#E73B3B] border-transparent"
+  end
+
+  defp color_variant("unbordered", "info") do
+    "bg-[#E5F0FF] text-[#004FC4] border-transparent"
+  end
+
+  defp color_variant("unbordered", "misc") do
+    "bg-[#FFE6FF] text-[#52059C] border-transparent"
+  end
+
+  defp color_variant("unbordered", "dawn") do
+    "bg-[#FFECDA] text-[#4D4137] border-transparent"
+  end
+
+  defp color_variant("unbordered", "light") do
+    "bg-[#E3E7F1] text-[#707483] border-transparent"
+  end
+
+  defp color_variant("unbordered", "dark") do
+    "bg-[#1E1E1E] text-white border-transparent"
+  end
+
+  defp color_variant("shadow", "white") do
+    "bg-white text-[#3E3E3E] border-[#DADADA] shadow-md"
+  end
+
+  defp color_variant("shadow", "primary") do
+    "bg-[#4363EC] text-white border-[#4363EC] shadow-md"
+  end
+
+  defp color_variant("shadow", "secondary") do
+    "bg-[#6B6E7C] text-white border-[#6B6E7C] shadow-md"
+  end
+
+  defp color_variant("shadow", "success") do
+    "bg-[#AFEAD0] text-[#227A52] border-[#AFEAD0] shadow-md"
+  end
+
+  defp color_variant("shadow", "warning") do
+    "bg-[#FFF8E6] text-[#FF8B08] border-[#FFF8E6] shadow-md"
+  end
+
+  defp color_variant("shadow", "danger") do
+    "bg-[#FFE6E6] text-[#E73B3B] border-[#FFE6E6] shadow-md"
+  end
+
+  defp color_variant("shadow", "info") do
+    "bg-[#E5F0FF] text-[#004FC4] border-[#E5F0FF] shadow-md"
+  end
+
+  defp color_variant("shadow", "misc") do
+    "bg-[#FFE6FF] text-[#52059C] border-[#FFE6FF] shadow-md"
+  end
+
+  defp color_variant("shadow", "dawn") do
+    "bg-[#FFECDA] text-[#4D4137] border-[#FFECDA] shadow-md"
+  end
+
+  defp color_variant("shadow", "light") do
+    "bg-[#E3E7F1] text-[#707483] border-[#E3E7F1] shadow-md"
+  end
+
+  defp color_variant("shadow", "dark") do
+    "bg-[#1E1E1E] text-white border-[#1E1E1E] shadow-md"
+  end
+
+  defp color_variant("transparent", "white") do
+    "bg-transparent text-white border-transparent"
+  end
+
+  defp color_variant("transparent", "primary") do
+    "bg-transparent text-[#4363EC] border-transparent"
+  end
+
+  defp color_variant("transparent", "secondary") do
+    "bg-transparent text-[#6B6E7C] border-transparent"
+  end
+
+  defp color_variant("transparent", "success") do
+    "bg-transparent text-[#227A52] border-transparent"
+  end
+
+  defp color_variant("transparent", "warning") do
+    "bg-transparent text-[#FF8B08] border-transparent"
+  end
+
+  defp color_variant("transparent", "danger") do
+    "bg-transparent text-[#E73B3B] border-transparent"
+  end
+
+  defp color_variant("transparent", "info") do
+    "bg-transparent text-[#6663FD] border-transparent"
+  end
+
+  defp color_variant("transparent", "misc") do
+    "bg-transparent text-[#52059C] border-transparent"
+  end
+
+  defp color_variant("transparent", "dawn") do
+    "bg-transparent text-[#4D4137] border-transparent"
+  end
+
+  defp color_variant("transparent", "light") do
+    "bg-transparent text-[#707483] border-transparent"
+  end
+
+  defp color_variant("transparent", "dark") do
+    "bg-transparent text-[#1E1E1E] border-transparent"
   end
 end
