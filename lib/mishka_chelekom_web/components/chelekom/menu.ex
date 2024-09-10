8b09efa3516c5115a751a1c082cefc026f6f3dde
Mona Aghili <28711325+MonaAghili@users.noreply.github.com>
2024-09-10 10:52:24 +0330

    vip

--- lib/mishka_chelekom_web/components/chelekom/menu.ex
+++ lib/mishka_chelekom_web/components/chelekom/menu.ex
@@ -7,12 +7,22 @@ defmodule MishkaChelekom.Menu do
   attr :id, :string, default: nil, doc: ""
   attr :class, :string, default: nil, doc: ""
   attr :menu_items, :list, default: [], doc: ""
+  attr :space, :string, default: "small", doc: ""
+  attr :padding, :string, default: "small", doc: ""
   slot :inner_block, doc: ""
   attr :rest, :global, doc: ""
 
   def menu(assigns) do
     ~H"""
-    <ul id={@id} class={@class} {@rest}>
+    <ul
+      id={@id}
+      class={[
+        padding_size(@padding),
+        space_class(@space),
+        @class
+      ]}
+      {@rest}
+    >
       <li :for={menu_item <- @menu_items}>
         <.button_link
           :if={Map.get(menu_item, :sub_items, []) == []}
@@ -37,4 +47,21 @@ defmodule MishkaChelekom.Menu do
     </ul>
     """
   end
+
+  defp padding_size("extra_small"), do: "p-2"
+  defp padding_size("small"), do: "p-2.5"
+  defp padding_size("medium"), do: "p-3"
+  defp padding_size("large"), do: "p-3.5"
+  defp padding_size("extra_large"), do: "p-4"
+  defp padding_size("none"), do: "p-0"
+  defp padding_size(params) when is_binary(params), do: params
+  defp padding_size(_), do: padding_size("extra_small")
+
+  defp space_class("extra_small"), do: "space-y-2"
+  defp space_class("small"), do: "space-y-3"
+  defp space_class("medium"), do: "space-y-4"
+  defp space_class("large"), do: "space-y-5"
+  defp space_class("extra_large"), do: "space-y-6"
+  defp space_class(params) when is_binary(params), do: params
+  defp space_class(_), do: "space-y-0"
 end
