edebb0997e733b86836d72dd2823bb640dbaa933
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-09-10 16:32:33 +0330

    VIP - add next and back

--- lib/mishka_chelekom_web/components/chelekom/carousel.ex
+++ lib/mishka_chelekom_web/components/chelekom/carousel.ex
@@ -1,9 +1,10 @@
 defmodule MishkaChelekom.Carousel do
   use Phoenix.Component
   import MishkaChelekomComponents
+  alias Phoenix.LiveView.JS
 
   @doc type: :component
-  attr :id, :string, default: nil, doc: ""
+  attr :id, :string, required: true, doc: ""
   attr :class, :string, default: nil, doc: ""
   attr :overlay, :string, default: "dark", doc: ""
   attr :size, :string, default: "large", doc: ""
@@ -24,12 +25,22 @@ defmodule MishkaChelekom.Carousel do
     attr :wrapper_class, :string
     attr :content_position, :string
     attr :class, :string
+    attr :active, :boolean, doc: ""
   end
 
   def carousel(assigns) do
+    assigns =
+      assign_new(assigns, :mounted_active_carousel, fn ->
+        Enum.find(assigns.slide, &Map.get(&1, :active))
+      end)
+
     ~H"""
     <div
       id={@id}
+      phx-mounted={
+        is_nil(@mounted_active_carousel) &&
+          unselect_carousel(@id, length(@slide)) |> select_carousel(@id, 1)
+      }
       class={[
         "relative h-full w-full",
         "[&_.slide]:absolute [&_.slide]:inset-0 [&_.slide]:opacity-0 [&_.slide.active-slide]:opacity-100",
@@ -37,53 +48,91 @@ defmodule MishkaChelekom.Carousel do
         text_position(@text_position),
         padding_size(@padding),
         color_class(@overlay),
-        size_class(@size),
+        size_class(@size)
       ]}
     >
-      <div :for={slide <- @slide} class={[
-        "slide h-full",
-        slide[:class]
-      ]}>
+      <div
+        :for={{slide, index} <- Enum.with_index(@slide, 1)}
+        id={"#{@id}-carousel-slide-#{index}"}
+        class={["slide h-full", slide[:class]]}
+        phx-mounted={
+          slide[:active] && unselect_carousel(@id, length(@slide)) |> select_carousel(@id, index)
+        }
+      >
         <div class="relative w-full">
           <div class={[
             "carousel-button-wrapper drop-shadow-2xl w-fit absolute top-0 bottom-0 left-0 p-5 flex justify-center items-center",
             "z-20 text-white transition-all ease-in-out duration-300 hover:bg-black/5"
           ]}>
-            <button class=""><.icon name="hero-chevron-left" class="size-5 md:size-7 lg:size-9" /></button>
+            <button
+              id={"#{@id}-carousel-next-btn-#{index}"}
+              phx-click={
+                index + 1 <= length(@slide) &&
+                  unselect_carousel(@id, length(@slide)) |> select_carousel(@id, index + 1)
+              }
+            >
+              <.icon name="hero-chevron-left" class="size-5 md:size-7 lg:size-9" />
+            </button>
           </div>
+
           <.link :if={!is_nil(slide[:link])} navigate={slide[:link]}>
-            <MishkaChelekom.Image.image class="max-w-full" src={slide[:image]} />
+            <MishkaChelekom.Image.image
+              class="max-w-full"
+              src={slide[:image]}
+              id={"#{@id}-carousel-slide-image-#{index}"}
+            />
           </.link>
 
-          <MishkaChelekom.Image.image :if={is_nil(slide[:link])} class="max-w-full" src={slide[:image]} />
+          <MishkaChelekom.Image.image
+            :if={is_nil(slide[:link])}
+            class="max-w-full"
+            src={slide[:image]}
+            id={"#{@id}-carousel-slide-image-#{index}"}
+          />
 
-          <div :if={!is_nil(slide[:title]) || !is_nil(slide[:description])} class="carousel-overlay absolute inset-0">
-            <div class={[
-              "description-wrapper h-full mx-auto flex flex-col gap-5",
-              content_position(slide[:content_position]),
-              slide[:wrapper_class]
-            ]}>
-              <div class={[
-                "carousel-title",
-                slide[:title_class] || "text-white"
-              ]}>
+          <div
+            :if={!is_nil(slide[:title]) || !is_nil(slide[:description])}
+            class="carousel-overlay absolute inset-0"
+            id={"#{@id}-carousel-slide-content-#{index}"}
+          >
+            <div
+              class={[
+                "description-wrapper h-full mx-auto flex flex-col gap-5",
+                content_position(slide[:content_position]),
+                slide[:wrapper_class]
+              ]}
+              id={"#{@id}-carousel-slide-content-position-#{index}"}
+            >
+              <div
+                :if={!is_nil(slide[:title])}
+                id={"#{@id}-carousel-slide-content-title-#{index}"}
+                class={["carousel-title", slide[:title_class] || "text-white"]}
+              >
                 <%= slide[:title] %>
               </div>
-              <p class={[
-                "carousel-description",
-                slide[:description_class]
-              ]}>
+              <p
+                :if={!is_nil(slide[:description])}
+                id={"#{@id}-carousel-slide-content-description-#{index}"}
+                class={["carousel-description", slide[:description_class]]}
+              >
                 <%= slide[:description] %>
               </p>
             </div>
           </div>
 
-          <div
-          class={[
+          <div class={[
             "carousel-button-wrapper drop-shadow-2xl w-fit absolute top-0 bottom-0 right-0 p-5 flex justify-center items-center",
             "z-20 text-white transition-all ease-in-out duration-300"
           ]}>
-            <button class=""><.icon name="hero-chevron-right" class="size-5 md:size-7 lg:size-9" /></button>
+            <button
+              id={"#{@id}-carousel-pervious-btn-#{index}"}
+              phx-click={
+                index - 1 != 0 &&
+                  unselect_carousel(@id, length(@slide)) |> select_carousel(@id, index - 1)
+              }
+            >
+              <.icon name="hero-chevron-right" class="size-5 md:size-7 lg:size-9" />
+            </button>
           </div>
         </div>
       </div>
@@ -91,19 +140,42 @@ defmodule MishkaChelekom.Carousel do
     """
   end
 
-  defp size_class("extra_small"), do: "text-xs [&_.description-wrapper]:max-w-80 [&_.carousel-title]:md:text-xl [&_.carousel-title]:md:text-3xl"
-  defp size_class("small"), do: "text-sm [&_.description-wrapper]:max-w-96 [&_.carousel-title]:md:text-xl [&_.carousel-title]:md:text-4xl"
-  defp size_class("medium"), do: "text-base [&_.description-wrapper]:max-w-xl [&_.carousel-title]:md:text-2xl [&_.carousel-title]:md:text-5xl"
-  defp size_class("large"), do: "text-lg [&_.description-wrapper]:max-w-2xl [&_.carousel-title]:md:text-3xl [&_.carousel-title]:md:text-6xl"
-  defp size_class("extra_large"), do: "text-xl [&_.description-wrapper]:max-w-3xl [&_.carousel-title]:md:text-3xl [&_.carousel-title]:md:text-7xl"
+  defp size_class("extra_small"),
+    do:
+      "text-xs [&_.description-wrapper]:max-w-80 [&_.carousel-title]:md:text-xl [&_.carousel-title]:md:text-3xl"
+
+  defp size_class("small"),
+    do:
+      "text-sm [&_.description-wrapper]:max-w-96 [&_.carousel-title]:md:text-xl [&_.carousel-title]:md:text-4xl"
+
+  defp size_class("medium"),
+    do:
+      "text-base [&_.description-wrapper]:max-w-xl [&_.carousel-title]:md:text-2xl [&_.carousel-title]:md:text-5xl"
+
+  defp size_class("large"),
+    do:
+      "text-lg [&_.description-wrapper]:max-w-2xl [&_.carousel-title]:md:text-3xl [&_.carousel-title]:md:text-6xl"
+
+  defp size_class("extra_large"),
+    do:
+      "text-xl [&_.description-wrapper]:max-w-3xl [&_.carousel-title]:md:text-3xl [&_.carousel-title]:md:text-7xl"
+
   defp size_class(params) when is_binary(params), do: params
   defp size_class(_), do: size_class("medium")
 
-  defp padding_size("extra_small"), do: "[&_.description-wrapper]:p-2.5 md:[&_.description-wrapper]:p-6"
+  defp padding_size("extra_small"),
+    do: "[&_.description-wrapper]:p-2.5 md:[&_.description-wrapper]:p-6"
+
   defp padding_size("small"), do: "[&_.description-wrapper]:p-3 md:[&_.description-wrapper]:p-7"
-  defp padding_size("medium"), do: "[&_.description-wrapper]:p-3.5 md:[&_.description-wrapper]:p-8"
+
+  defp padding_size("medium"),
+    do: "[&_.description-wrapper]:p-3.5 md:[&_.description-wrapper]:p-8"
+
   defp padding_size("large"), do: "[&_.description-wrapper]:p-4 md:[&_.description-wrapper]:p-9"
-  defp padding_size("extra_large"), do: "[&_.description-wrapper]:p-5 md:[&_.description-wrapper]:p-10"
+
+  defp padding_size("extra_large"),
+    do: "[&_.description-wrapper]:p-5 md:[&_.description-wrapper]:p-10"
+
   defp padding_size(params) when is_binary(params), do: params
   defp padding_size(_), do: padding_size("medium")
 
@@ -142,7 +214,6 @@ defmodule MishkaChelekom.Carousel do
   end
 
   defp text_position(_), do: text_position("center")
-
 
   defp color_class("white") do
     "[&_.carousel-overlay]:bg-white/30 text-[#3E3E3E] hover:[&_.carousel-button-wrapper]:bg-white/5"
@@ -188,4 +259,24 @@ defmodule MishkaChelekom.Carousel do
     "[&_.carousel-overlay]:bg-[#1E1E1E]/30 text-white hover:[&_.carousel-button-wrapper]:bg-[#1E1E1E]/5"
   end
 
+  def select_carousel(js \\ %JS{}, id, count) when is_binary(id) do
+    JS.add_class(js, "active-slide",
+      to: "##{id}-carousel-slide-#{count}",
+      transition: "duration-0"
+    )
+    |> JS.remove_attribute("disabled", to: "##{id}-carousel-pervious-btn-#{count}")
+    |> JS.remove_attribute("disabled", to: "##{id}-carousel-next-btn-#{count}")
+  end
+
+  def unselect_carousel(js \\ %JS{}, id, count) do
+    Enum.reduce(1..count, js, fn item, acc ->
+      acc
+      |> JS.set_attribute({"disabled", "true"}, to: "##{id}-carousel-pervious-btn-#{item}")
+      |> JS.set_attribute({"disabled", "true"}, to: "##{id}-carousel-next-btn-#{item}")
+      |> JS.remove_class("active-slide",
+        to: "##{id}-carousel-slide-#{item}",
+        transition: "duration-0"
+      )
+    end)
+  end
 end
