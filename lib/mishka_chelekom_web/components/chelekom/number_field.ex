e200046b11d29400d32dbe4ade6ccfc38e14d19b
Mona Aghili <28711325+MonaAghili@users.noreply.github.com>
2024-09-15 18:26:37 +0330

    add attributes

--- lib/mishka_chelekom_web/components/chelekom/number_field.ex
+++ lib/mishka_chelekom_web/components/chelekom/number_field.ex
@@ -37,7 +37,7 @@ defmodule MishkaChelekom.NumberField do
 
   attr :rest, :global,
     include:
-      ~w(autocomplete disabled form list min max pattern placeholder readonly required size inputmode inputmode step)
+      ~w(autocomplete disabled form list min max pattern placeholder readonly required size inputmode inputmode step title)
 
   @spec number_field(map()) :: Phoenix.LiveView.Rendered.t()
   def number_field(%{field: %Phoenix.HTML.FormField{} = field} = assigns) do
--- lib/mishka_chelekom_web/components/chelekom/password_field.ex
+++ lib/mishka_chelekom_web/components/chelekom/password_field.ex
@@ -39,7 +39,7 @@ defmodule MishkaChelekom.PasswordField do
 
   attr :rest, :global,
     include:
-      ~w(autocomplete disabled form maxlength minlength pattern placeholder readonly required size spellcheck inputmode)
+      ~w(autocomplete disabled form maxlength minlength pattern placeholder readonly required size spellcheck inputmode title)
 
   @spec password_field(map()) :: Phoenix.LiveView.Rendered.t()
   def password_field(%{field: %Phoenix.HTML.FormField{} = field} = assigns) do
