65395c32201910f3979188b631cab9e261436e0f
Mona Aghili <28711325+MonaAghili@users.noreply.github.com>
2024-09-16 11:42:15 +0330

    vip

--- lib/mishka_chelekom_web/components/chelekom/tel_field.ex
+++ lib/mishka_chelekom_web/components/chelekom/tel_field.ex
@@ -36,7 +36,7 @@ defmodule MishkaChelekom.TelField do
 
   attr :rest, :global,
     include: ~w(autocomplete disabled form list maxlength minlength pattern placeholder
-        readonly required size inputmode inputmode step title autofocus)
+        readonly required size inputmode inputmode title autofocus)
 
   @spec tel_field(map()) :: Phoenix.LiveView.Rendered.t()
   def tel_field(%{field: %Phoenix.HTML.FormField{} = field} = assigns) do
