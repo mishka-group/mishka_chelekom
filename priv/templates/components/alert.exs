3fc9bb19d3bb47f02a1a58c9da9a8629726a3f5c
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-09-25 14:11:04 +0330

    Add web_module to component task

---
+++ priv/templates/components/alert.exs
@@ -0,0 +1,28 @@
+[
+  alert: [
+    name: "alert",
+    args: [
+      variant: ["default","outline","shadow","unbordered"],
+      color: [
+        "info",
+        "danger",
+        "success",
+        "white",
+        "primary",
+        "secondary",
+        "misc",
+        "warning",
+        "dark",
+        "light",
+        "dawn",
+        "error"
+      ],
+      size: ["extra_small", "small", "medium", "large", "extra_large"],
+      rounded: ["extra_small", "small", "medium", "large", "extra_large", "full", "none"],
+      type: ["flash", "flash_group", "alert"],
+      module: ""
+    ],
+    optional: [],
+    necessary: []
+  ]
+]
