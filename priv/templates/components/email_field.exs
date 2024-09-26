91e77a9addeeaae427ce1c0076d6d2a90c54093f
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-09-26 23:40:10 +0330

    Add eex entry file of email_field

--- priv/templates/components/email_field.exs
+++ priv/templates/components/email_field.exs
@@ -1,8 +1,8 @@
 [
-  drawer: [
-    name: "drawer",
+  email_field: [
+    name: "email_field",
     args: [
-      variant: ["default", "outline", "transparent", "shadow", "unbordered"],
+      variant: ["default", "outline", "unbordered", "shadow", "transparent"],
       color: [
         "white",
         "primary",
@@ -17,6 +17,8 @@
         "dawn"
       ],
       size: ["extra_small", "small", "medium", "large", "extra_large"],
+      space: ["extra_small", "small", "medium", "large", "extra_large"],
+      rounded: ["extra_small", "small", "medium", "large", "extra_large", "full"],
       module: ""
     ],
     optional: [],
