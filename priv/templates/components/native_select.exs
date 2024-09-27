1df535587043450a3a74a809f0e5138590f3cee8
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-09-27 20:48:14 +0330

    Add entry eex file of native_select

---
+++ priv/templates/components/native_select.exs
@@ -0,0 +1,27 @@
+[
+  native_select: [
+    name: "native_select",
+    args: [
+      variant: ["default", "outline", "transparent", "shadow", "unbordered"],
+      color: [
+        "white",
+        "primary",
+        "secondary",
+        "dark",
+        "success",
+        "warning",
+        "danger",
+        "info",
+        "light",
+        "misc",
+        "dawn"
+      ],
+      space: ["extra_small", "small", "medium", "large", "extra_large"],
+      rounded: ["extra_small", "small", "medium", "large", "extra_large", "full"],
+      size: ["extra_small", "small", "medium", "large", "extra_large"],
+      module: ""
+    ],
+    optional: [],
+    necessary: []
+  ]
+]
