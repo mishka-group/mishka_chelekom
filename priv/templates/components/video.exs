10197f6ad5fe169a24bc259477d5d1db78fb8294
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-09-29 02:27:48 +0330

    Add some conditions of video eex

--- priv/templates/components/video.exs
+++ priv/templates/components/video.exs
@@ -2,20 +2,6 @@
   video: [
     name: "video",
     args: [
-      color: [
-        "white",
-        "primary",
-        "secondary",
-        "dark",
-        "success",
-        "warning",
-        "danger",
-        "info",
-        "light",
-        "misc",
-        "dawn",
-        "silver"
-      ],
       rounded: ["extra_small", "small", "medium", "large", "extra_large", "none"],
       module: ""
     ],
