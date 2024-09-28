aebbffa5f47319d82a4902139c216988cdb79c03
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-09-29 01:22:06 +0330

    Add entry eex of tooltip

---
+++ priv/templates/components/tooltip.exs
@@ -0,0 +1,27 @@
+[
+  tooltip: [
+    name: "tooltip",
+    args: [
+      variant: ["default", "shadow"],
+      color: [
+        "white",
+        "primary",
+        "secondary",
+        "dark",
+        "success",
+        "warning",
+        "danger",
+        "info",
+        "light",
+        "misc",
+        "dawn"
+      ],
+      size: ["extra_small", "small", "medium", "large", "extra_large"],
+      space: ["extra_small", "small", "medium", "large", "extra_large"],
+      padding: ["extra_small", "small", "medium", "large", "extra_large", "none"],
+      module: ""
+    ],
+    optional: [],
+    necessary: []
+  ]
+]
