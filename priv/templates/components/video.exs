d8418bf895655df9ceaf725ef59bb9fc0db7c4b4
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-09-29 02:24:37 +0330

    Add video eex

---
+++ priv/templates/components/video.exs
@@ -0,0 +1,25 @@
+[
+  video: [
+    name: "video",
+    args: [
+      color: [
+        "white",
+        "primary",
+        "secondary",
+        "dark",
+        "success",
+        "warning",
+        "danger",
+        "info",
+        "light",
+        "misc",
+        "dawn",
+        "silver"
+      ],
+      rounded: ["extra_small", "small", "medium", "large", "extra_large", "none"],
+      module: ""
+    ],
+    optional: [],
+    necessary: []
+  ]
+]
