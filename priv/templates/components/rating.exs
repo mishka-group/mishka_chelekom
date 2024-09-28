77a29932f78391750b6879362a02c726242b97f0
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-09-28 10:50:23 +0330

    Add rating eex file entry level

---
+++ priv/templates/components/rating.exs
@@ -0,0 +1,24 @@
+[
+  rating: [
+    name: "rating",
+    args: [
+      color: [
+        "white",
+        "primary",
+        "secondary",
+        "dark",
+        "success",
+        "warning",
+        "danger",
+        "info",
+        "light",
+        "misc",
+        "dawn"
+      ],
+      size: ["extra_small", "small", "medium", "large", "extra_large"],
+      module: ""
+    ],
+    optional: [],
+    necessary: []
+  ]
+]
