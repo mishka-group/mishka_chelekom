31b75f76fa5453e03e39ac9b7c3bcb300c79eef1
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-09-28 17:47:18 +0330

    Add entry eex file of spinner

---
+++ priv/templates/components/spinner.exs
@@ -0,0 +1,38 @@
+[
+  spinner: [
+    name: "spinner",
+    args: [
+      variant: ["default", "unbordered", "shadow"],
+      color: [
+        "white",
+        "primary",
+        "secondary",
+        "dark",
+        "success",
+        "warning",
+        "danger",
+        "info",
+        "light",
+        "misc",
+        "dawn"
+      ],
+      size: [
+        "extra_small",
+        "small",
+        "medium",
+        "large",
+        "extra_large",
+        "double_large",
+        "triple_large",
+        "quadruple_large"
+      ],
+      space: ["extra_small", "small", "medium", "large", "extra_large"],
+      padding: ["extra_small", "small", "medium", "large", "extra_large", "none"],
+      rounded: ["extra_small", "small", "medium", "large", "extra_large", "full"],
+      type: ["default", "pinging", "dots", "bars"],
+      module: ""
+    ],
+    optional: [],
+    necessary: []
+  ]
+]
