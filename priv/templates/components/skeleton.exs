534590517a89d7bbcb29bcf82e17292ba12e1d26
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-09-28 12:57:06 +0330

    Add  entry eex of skeleton

---
+++ priv/templates/components/skeleton.exs
@@ -0,0 +1,24 @@
+[
+  skeleton: [
+    name: "skeleton",
+    args: [
+      color: [
+        "white",
+        "primary",
+        "secondary",
+        "dark",
+        "success",
+        "warning",
+        "danger",
+        "info",
+        "light",
+        "misc",
+        "dawn"
+      ],
+      rounded: ["extra_small", "small", "medium", "large", "extra_large", "full", "none"],
+      module: ""
+    ],
+    optional: [],
+    necessary: []
+  ]
+]
