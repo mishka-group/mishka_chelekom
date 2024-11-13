ea5e57b1d4edc08c1667bedb2edc8d90a4b0b19d
Mona Aghili <28711325+MonaAghili@users.noreply.github.com>
2024-11-13 16:38:23 +0330

    Add helpers to `exs` avatar

--- priv/templates/components/avatar.exs
+++ priv/templates/components/avatar.exs
@@ -20,6 +20,7 @@
       rounded: ["extra_small", "small", "medium", "large", "extra_large", "full", "none"],
       type: ["avatar", "avatar_group"],
       only: ["avatar", "avatar_group"],
+      helpers: [],
       module: ""
     ],
     optional: [],
