4c1e9ebfbdce78c08c82ae1b2f5c6e6d97460f50
Mona Aghili <28711325+MonaAghili@users.noreply.github.com>
2024-11-14 12:40:55 +0330

    Chore: Modify colors of spinner and add dark mode

--- priv/templates/components/spinner.exs
+++ priv/templates/components/spinner.exs
@@ -3,15 +3,14 @@
     name: "spinner",
     args: [
       color: [
-        "white",
+        "natural",
         "primary",
         "secondary",
-        "dark",
         "success",
         "warning",
         "danger",
         "info",
-        "light",
+        "silver",
         "misc",
         "dawn"
       ],
