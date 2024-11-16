ce19a4b59d462e1b8aa4952134629d2878e6bb40
Mona Aghili <28711325+MonaAghili@users.noreply.github.com>
2024-11-16 13:28:24 +0330

    Add helpers to `exs` file

--- priv/templates/components/blockquote.exs
+++ priv/templates/components/blockquote.exs
@@ -20,6 +20,7 @@
       space: ["extra_small", "small", "medium", "large", "extra_large"],
       padding: ["extra_small", "small", "medium", "large", "extra_large", "none"],
       only: ["blockquote"],
+      helpers: [],
       module: ""
     ],
     optional: [],
