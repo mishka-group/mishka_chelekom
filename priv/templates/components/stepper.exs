05a8283ae057c08385fdd3e654393224cd9d019d
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-12-05 18:28:28 +0330

    Fix typo

--- priv/templates/components/stepper.exs
+++ priv/templates/components/stepper.exs
@@ -18,6 +18,7 @@
       space: ["extra_small", "small", "medium", "large", "extra_large"],
       type: ["stepper", "stepper_section"],
       only: ["stepper", "stepper_section"],
+      helpers: [],
       module: ""
     ],
     optional: [],
