927a00638a0e0d95dd1a426a0f1ff024b58c5ad9
Mona Aghili <28711325+MonaAghili@users.noreply.github.com>
2025-02-11 13:55:37 +0330

    Feat: add scroll area component

---
+++ priv/components/scroll_area.exs
@@ -0,0 +1,13 @@
+[
+  rating: [
+    name: "scroll_area",
+    args: [
+      padding: ["extra_small", "small", "medium", "large", "extra_large", "none"],
+      only: ["scroll_area"],
+      helpers: [],
+      module: ""
+    ],
+    optional: [],
+    necessary: []
+  ]
+]
