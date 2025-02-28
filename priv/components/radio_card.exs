8124b89957717c5864a72aadba6f1fce6c925ef6
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2025-02-28 12:25:29 +0330

    Update radio_card.exs

--- priv/components/radio_card.exs
+++ priv/components/radio_card.exs
@@ -20,7 +20,7 @@
       ],
       size: ["extra_small", "small", "medium", "large", "extra_large"],
       space: ["extra_small", "small", "medium", "large", "extra_large", "none"],
-      padding: ["extra_small", "small", "medium", "large", "extra_large",],
+      padding: ["extra_small", "small", "medium", "large", "extra_large"],
       rounded: ["extra_small", "small", "medium", "large", "extra_large"],
       only: ["radio_card"],
       helpers: [],
