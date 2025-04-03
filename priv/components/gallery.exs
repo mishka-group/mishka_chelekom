23e6f3191ec6e87ceea511c50e200613e54baaed
Mona Aghili <28711325+MonaAghili@users.noreply.github.com>
2025-04-03 14:25:11 +0330

    Feat: add filterable gallery component

--- priv/components/gallery.exs
+++ priv/components/gallery.exs
@@ -3,8 +3,8 @@
     name: "gallery",
     args: [
       rounded: ["extra_small", "small", "medium", "large", "extra_large", "none"],
-      type: ["gallery", "gallery_media"],
-      only: ["gallery", "gallery_media"],
+      type: ["gallery", "gallery_media", "filterable_gallery"],
+      only: ["gallery", "gallery_media", "filterable_gallery"],
       helpers: [],
       module: ""
     ],
