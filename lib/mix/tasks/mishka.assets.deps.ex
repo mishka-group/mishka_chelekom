ff1d55e1397944c906eb45433498ed39782dfe01
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2025-05-26 01:31:51 +0330

    vip

--- lib/mix/tasks/mishka.assets.deps.ex
+++ lib/mix/tasks/mishka.assets.deps.ex
@@ -31,30 +31,6 @@ defmodule Mix.Tasks.Mishka.Assets.Deps.Docs do
   end
 end
 
-defmodule Mix.Tasks.Mishka.Assets.Install do
-  @shortdoc "Runs package manager install in assets directory"
-  @moduledoc false
-
-  use Mix.Task
-
-  @impl Mix.Task
-  def run([package_manager]) do
-    File.cd!("assets", fn ->
-      IO.puts("\n#{IO.ANSI.cyan()}Running #{package_manager} install...#{IO.ANSI.reset()}")
-
-      case System.cmd(package_manager, ["install"], into: IO.stream(:stdio, :line)) do
-        {_, 0} ->
-          IO.puts("#{IO.ANSI.green()}✓ Dependencies installed successfully!#{IO.ANSI.reset()}")
-
-        {_, exit_code} ->
-          IO.puts(
-            "#{IO.ANSI.red()}✗ Failed to install dependencies (exit code: #{exit_code})#{IO.ANSI.reset()}"
-          )
-      end
-    end)
-  end
-end
-
 if Code.ensure_loaded?(Igniter) do
   defmodule Mix.Tasks.Mishka.Assets.Deps do
     @shortdoc "#{__MODULE__.Docs.short_doc()}"
