2a6ccf01c89e8dd2716b1c191ef64398e2229590
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2025-12-01 23:54:44 +0330

    Bump version to 0.0.9-alpha.5 and update package files
    Updated the project version to 0.0.9-alpha.5 and added 'usage-rules.md' and 'usage-rules' to the package files list. Also updated the changelog to mention the addition of usage rules for all components and JS Hooks.

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule MishkaChelekom.MixProject do
   use Mix.Project
 
-  @version "0.0.9-alpha.4"
+  @version "0.0.9-alpha.5"
   @source_url "https://github.com/mishka-group/mishka_chelekom"
 
   def project do
@@ -54,7 +54,7 @@ defmodule MishkaChelekom.MixProject do
   defp package() do
     [
       extra: %{igniter_only: ["dev"]},
-      files: ~w(lib priv .formatter.exs mix.exs LICENSE README*),
+      files: ~w(lib priv .formatter.exs mix.exs LICENSE README* usage-rules.md usage-rules),
       licenses: ["Apache-2.0"],
       maintainers: ["Shahryar Tavakkoli", "Mona Aghili", "Arian Alijani"],
       links: %{
