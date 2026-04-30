4b7dfb0dc5839a29765356eb5adc89e235de8227
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2026-04-30 16:56:22 +0330

    Add entry level of Stat component
    Update mix.exs

---
+++ priv/components/stat.exs
@@ -0,0 +1,33 @@
+[
+  stat: [
+    name: "stat",
+    category: "general",
+    doc_url: "https://mishka.tools/chelekom/docs/stat",
+    args: [
+      variant: ["default", "shadow", "bordered", "gradient", "base"],
+      color: [
+        "base",
+        "natural",
+        "white",
+        "primary",
+        "secondary",
+        "dark",
+        "success",
+        "warning",
+        "danger",
+        "info",
+        "silver",
+        "misc",
+        "dawn"
+      ],
+      size: ["extra_small", "small", "medium", "large", "extra_large"],
+      padding: ["none", "extra_small", "small", "medium", "large", "extra_large"],
+      rounded: ["none", "extra_small", "small", "medium", "large", "extra_large", "full"],
+      only: ["stat", "stat_group"],
+      helpers: [],
+      module: ""
+    ],
+    optional: [],
+    necessary: ["icon"]
+  ]
+]
